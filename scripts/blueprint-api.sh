9ed4fe63356df60c0291a9efce6ebcad1c1b794f
Yifeng Jiang <uprushworld@gmail.com>
2015-05-18 19:27:12 +0900

    fix zk host bug

--- scripts/blueprint-api.sh
+++ scripts/blueprint-api.sh
@@ -13,5 +13,5 @@
 # curl -H "X-Requested-By: ambari" -X POST -d @blueprints/sandbox/sandbox.json -u admin:admin localhost:8080/api/v1/blueprints/sandbox
 
 # create cluster
-# curl -H "X-Requested-By: ambari" -X POST -d @blueprints/sandbox/cluster-template.json -u admin:admin localhost:8080/api/v1/clusters/sandbox
+# curl -H "X-Requested-By: ambari" -X POST -d @blueprints/sandbox/snadbox-template.json -u admin:admin localhost:8080/api/v1/clusters/sandbox
 
